95e8f7e466ebf8200ba2e3d857d79c31f6b97b7c
koncsard-eszter <koncsardesz@gmail.com>
2025-05-08 00:23:44 +0300

    Extract frequency bands using FFT from signal data

--- feature_extractor.jl
+++ feature_extractor.jl
@@ -3,15 +3,28 @@ Pkg.add("MAT");
 Pkg.add("EEGToolkit");
 Pkg.add("Plots");
 Pkg.add("Statistics");
+Pkg.add("FFTW");
 using MAT
 using EEGToolkit
 using Plots
 using Statistics
 using Printf
+using FFTW
+
 
 struct CustomEEG
     signals::Dict{String, TimeSeries}
 end
+
+
+frequency_bands = Dict(
+    "delta" => (0, 4),
+    "theta" => (4, 8),
+    "alpha" => (8, 14),
+    "beta" => (15, 30),
+    "low_gamma" => (30, 100),
+    "high_gamma" => (100, 200)
+)
 
 
 function load_eeg(file::String, segment_name::String)
@@ -60,18 +73,49 @@ function get_features_of_a_signal(signal::Vector{Float64})
 end
 
 
-function print_features(signal_identifier, min, max, mean, std)
+function print_features(signal_identifier, min, max, mean, std, band_powers)
     @printf("%s: min = %s, max = %s, mean = %s, std = %s \n", signal_identifier, min, max, mean, std)
+    for (idx, (band_name, band_range)) in enumerate(frequency_bands)
+        band_start = band_range[1]
+        band_end = band_range[2]
+        println("\t\t$band_name ([$band_start - $band_end]): $(band_powers[idx])")
+    end
 end
 
 
-function compare_two_signal_metadata(signal_1::Vector{Float64}, signal_2::Vector{Float64})
-    min_of_first, max_of_first, mean_of_first, std_of_first = get_features_of_a_signal(signal_1)
+function compare_two_signal_metadata(signal_1::TimeSeries, signal_2::TimeSeries)
+    min_of_first, max_of_first, mean_of_first, std_of_first = get_features_of_a_signal(signal_1.x)
+    band_powers_1 = get_frequency_bands(signal_1.x, signal_1.fs)
 
-    min_of_second, max_of_second, mean_of_second, std_of_second = get_features_of_a_signal(signal_2)
+    min_of_second, max_of_second, mean_of_second, std_of_second = get_features_of_a_signal(signal_2.x)
+    band_powers_2 = get_frequency_bands(signal_2.x, signal_2.fs)
 
-    print_features("Interictal segment", min_of_first, max_of_first, mean_of_first, std_of_first)
-    print_features("Preictal segment", min_of_second, max_of_second, mean_of_second, std_of_second)
+
+    print_features("Interictal segment", min_of_first, max_of_first, mean_of_first, std_of_first, band_powers_1)
+    print_features("Preictal segment", min_of_second, max_of_second, mean_of_second, std_of_second, band_powers_2)
+end
+
+
+function get_frequency_bands(signal, sampling_frequency)
+    fft_signal = fft(signal)
+    len_of_signal = length(signal)
+    n = 1:len_of_signal
+    T = len_of_signal/sampling_frequency
+    freqs = n / T
+    half_size = div(len_of_signal,2)
+    one_sided_freqs = freqs[1:half_size]
+    amplitudes = abs.(fft_signal[1:half_size])
+
+    num_bands = length(frequency_bands)
+    band_powers = zeros(Float64, num_bands)
+    for (idx, (band_name, band_range)) in enumerate(frequency_bands)
+        band_start = band_range[1]
+        band_end = band_range[2]
+        band_indices= findall(f -> band_start <= f < band_end, one_sided_freqs)
+        band_power = mean(amplitudes[band_indices])^2
+        band_powers[idx] = band_power
+    end
+    return band_powers
 end
 
 
@@ -88,9 +132,10 @@ function main()
 
     channel = "NVC1202_32_002_Ecog_c013"
 
+    
     # print(interictal_eeg.signals[channel].fs) - sampling frequency
     # print(interictal_eeg.signals[channel].x) - signal
-    compare_two_signal_metadata(interictal_eeg.signals[channel].x, preictal_eeg.signals[channel].x)
+    compare_two_signal_metadata(interictal_eeg.signals[channel], preictal_eeg.signals[channel])
 
     # print(channels)
     # channels = keys(eeg.signals)
