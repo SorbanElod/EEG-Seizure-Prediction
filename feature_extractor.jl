82cf4b808b5ba35ea5dc2e6972b68d775eb59096
koncsard-eszter <koncsardesz@gmail.com>
2025-05-09 00:43:36 +0300

    apply Hanning window before FFT and extract frequency bands from three segments per channel

--- feature_extractor.jl
+++ feature_extractor.jl
@@ -5,6 +5,7 @@ Pkg.add("Plots");
 Pkg.add("Statistics");
 Pkg.add("FFTW");
 Pkg.add("OrderedCollections");
+Pkg.add("DSP") # for Hanning windowing
 using MAT
 using EEGToolkit
 using Plots
@@ -12,13 +13,14 @@ using Statistics
 using Printf
 using FFTW
 using OrderedCollections
+using DSP
 
 
 struct CustomEEG
     signals::OrderedDict{String, TimeSeries}
 end
 
-frequency_bands = OrderedDict(
+const frequency_bands = OrderedDict(
     "delta" => (0, 4),
     "theta" => (4, 8),
     "alpha" => (8, 14),
@@ -74,12 +76,16 @@ function extract_statistical_features(signal::Vector{Float64})
 end
 
 
-function extract_frequency_bands(signal, sampling_frequency)
-    fft_signal = fft(signal)
-    len_of_signal = length(signal)
-    n = 1:len_of_signal
+function extract_frequency_bands_from_signal(signal, sampling_frequency)
+    # Applying Hanning window to the signal before the FFT 
+    hanning_window = hanning(length(signal))
+    windowed_signal = signal .* hanning_window
+
+    fft_signal = fft(windowed_signal)
+    len_of_signal = length(windowed_signal)
+    n = 0:len_of_signal
     T = len_of_signal/sampling_frequency
-    freqs = n / T
+    freqs = n ./ T
     half_size = div(len_of_signal,2)
     one_sided_freqs = freqs[1:half_size]
     amplitudes = abs.(fft_signal[1:half_size])
@@ -90,11 +96,31 @@ function extract_frequency_bands(signal, sampling_frequency)
         band_start = band_range[1]
         band_end = band_range[2]
         band_indices= findall(f -> band_start <= f < band_end, one_sided_freqs)
-        band_power = mean(amplitudes[band_indices])^2
-        band_powers[idx] = band_power
+        band_power = mean(amplitudes[band_indices].^2)
+        logarithmic_band_power = log10(band_power)
+        band_powers[idx] = logarithmic_band_power
     end
     return band_powers
 end
+
+
+function extract_frequency_bands_from_three_segments(signal, sampling_frequency)
+    length_of_signal = length(signal)
+    mid_point = div(length_of_signal, 2)
+    length_of_segment_sec = 0.5
+    values_in_segment = map(Int, length_of_segment_sec * sampling_frequency)
+    half_seg = div(values_in_segment, 2)
+    segment_from_start = signal[1:values_in_segment]
+    segment_from_middle = signal[(mid_point - half_seg + 1):(mid_point + half_seg + 1)]
+    segment_from_end = signal[(length_of_signal-values_in_segment + 1):length_of_signal]
+
+    frequency_bands = Vector{Float64}()
+    append!(frequency_bands, extract_frequency_bands_from_signal(segment_from_start, sampling_frequency))
+    append!(frequency_bands, extract_frequency_bands_from_signal(segment_from_middle, sampling_frequency))
+    append!(frequency_bands, extract_frequency_bands_from_signal(segment_from_end, sampling_frequency))
+    return frequency_bands
+end
+
 
 
 function print_features(signal_identifier, min, max, mean, std, band_powers)
@@ -109,10 +135,10 @@ end
 
 function compare_features_of_two_signal(signal_1::TimeSeries, signal_2::TimeSeries)
     min_of_first, max_of_first, mean_of_first, std_of_first = extract_statistical_features(signal_1.x)
-    band_powers_1 = extract_frequency_bands(signal_1.x, signal_1.fs)
+    band_powers_1 = extract_frequency_bands_from_three_segments(signal_1.x, signal_1.fs)
 
     min_of_second, max_of_second, mean_of_second, std_of_second = extract_statistical_features(signal_2.x)
-    band_powers_2 = extract_frequency_bands(signal_2.x, signal_2.fs)
+    band_powers_2 = extract_frequency_bands_from_three_segments(signal_2.x, signal_2.fs)
 
     print_features("Interictal segment", min_of_first, max_of_first, mean_of_first, std_of_first, band_powers_1)
     print_features("Preictal segment", min_of_second, max_of_second, mean_of_second, std_of_second, band_powers_2)
@@ -121,21 +147,40 @@ end
 
 function extract_features_from_signal(signal::TimeSeries)
     min, max, mean, std = extract_statistical_features(signal.x)
-    band_powers = extract_frequency_bands(signal.x, signal.fs)    
+    band_powers = extract_frequency_bands_from_three_segments(signal.x, signal.fs)    
     features = [min, max, mean, std, band_powers...]
     return features
 end
 
 
-function extract_features_from_all_channels(eeg::CustomEEG)
+function extract_statistical_and_frequency_features_from_all_channels(eeg::CustomEEG)
     channels = collect(keys(eeg.signals))
     features = Vector{Float64}()
     for channel in channels
         append!(features, extract_features_from_signal(eeg.signals[channel]))        
     end
-    println(features)
+    return features
 end
 
+
+function extract_statistical_features_from_all_channels(eeg::CustomEEG)
+    channels = collect(keys(eeg.signals))
+    features = Vector{Float64}()
+    for channel in channels
+        append!(features, extract_statistical_features(eeg.signals[channel].x))        
+    end
+    return features
+end
+
+
+function extract_frequency_features_from_all_channels(eeg::CustomEEG)
+    channels = collect(keys(eeg.signals))
+    features = Vector{Float64}()
+    for channel in channels
+        append!(features, extract_frequency_bands_from_three_segments(eeg.signals[channel].x, eeg.signals[channel].fs))        
+    end
+    return features
+end
 
 
 function main()
@@ -154,8 +199,9 @@ function main()
     # print(interictal_eeg.signals[channel].fs) - sampling frequency
     # print(interictal_eeg.signals[channel].x) - signal
     compare_features_of_two_signal(interictal_eeg.signals[channel], preictal_eeg.signals[channel])
-    extract_features_from_all_channels(interictal_eeg)
-    extract_features_from_all_channels(preictal_eeg)
+    interictal_features = extract_statistical_and_frequency_features_from_all_channels(interictal_eeg)
+    preictal_features = extract_statistical_and_frequency_features_from_all_channels(preictal_eeg)
+    println(preictal_features)
     # print(channels)
     # channels = keys(eeg.signals)
     # print(eeg.signals["NVC1202_32_002_Ecog_c013"])
